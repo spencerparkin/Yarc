9ddef5b5a6c6863a442707e6eefdd56eaee9b264
SpencerParkin <spencerparkin@outlook.com>
2020-07-24 18:25:25 -0600

    Add note.

--- Source/yarc_simple_client.h
+++ Source/yarc_simple_client.h
@@ -13,6 +13,7 @@ namespace Yarc
 {
 	// TODO: Add connection dispositions: normal, persistent or lazy.
 	//       The lazy disposition would make scaling Redis cluster to many nodes more practical.
+	//       Note this may mean removing the connect, disconnect and is-connected functions.
 	class YARC_API SimpleClient : public ClientInterface
 	{
 	public:
